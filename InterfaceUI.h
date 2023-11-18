0a4cf240667faac86902091393a9749f476dada7
lwxin0805 <137153563+lwxin0805@users.noreply.github.com>
2023-11-18 19:15:03 +0800

    第一次修改
    尝试把代码模块化分类，第一个分类的模块是界面，把界面的函数分成了单独的cpp和h

---
+++ InterfaceUI.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class InterfaceUI
+{
+public:
+	void DefaultChoice();
+	bool AgainService();
+	void MainInterface();
+	void MainMenu(string& phonenumber);
+	void AddFriendInterface(string& phonenumber);
+	void ChatPersonalOrGroup(string& phonenumber);
+	void ChatInterface(string& phonenumber);
+	void ChatHistoryOneOrGroup(string& phonenumber);
+	void ChatHistoryInterface(string& phonenumber, char& index1);
+	void CheckPYQOneOrMany(string& phonenumber);
+	void CheckPYQOne(string& phonenumber);
+	void CheckPYQ(string& phonenumber);
+	void PYQInterface(string& phonenumber);
+	void SettingInterface(string& phonenumber);
+	void AddFriendMethod(string& phonenumber);
+	void PersonalSetting(string& phonenumber);
+};
